c60931bce6e698c757a92ef47bc7790b549e1a27
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-09-15 09:29:45 +0600

    readme file edited, project  features added

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Courses = ({ courses, handleAddToCourse }) => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-auto place-items-center gap-6 py-8'>
+        <div className='grid grid-cols-1 xl:grid-cols-3 mx-auto place-items-center gap-6 py-8'>
             {
                 courses.map(course => <Course key={course.id} course={course} handleAddToCourse={handleAddToCourse} ></Course>)
             }
--- src/components/SelectedCourse/SelectedCourse.jsx
+++ src/components/SelectedCourse/SelectedCourse.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const SelectedCourse = ({ selectedCourses, totalCredit, remainingCredit, price }) => {
     console.log(selectedCourses);
     return (
-        <div className='card w-[300px] h-fit my-8 bg-[#fff] shadow-xl p-6 space-y-4 rounded-xl p'>
+        <div className='card w-[300px] h-fit my-8 bg-[#fff] shadow-xl p-6 space-y-2 rounded-xl p'>
             <h1 className='text-lg text-[#2F80ED] font-bold '>Credit Hour Remaining {remainingCredit} hr</h1>
             <div className="divider"></div>
             <h3 className='text-xl font-bold'>Course Name</h3>
@@ -14,6 +14,7 @@ const SelectedCourse = ({ selectedCourses, totalCredit, remainingCredit, price }
             }
             <div className="divider"></div>
             <h5 className='text-base font-medium'>Total Credit Hour : {totalCredit} </h5>
+            <div className="divider"></div>
             <h5 className='text-base font-medium'>Total Price : {price} USD</h5>
         </div>
     );
